295a004a704b6240b07250d0b9ee00e2e47120ea
Vitek Shanel <vit.shanel@gmail.com>
2023-10-10 11:08:09 +0200

    init comm

---
+++ test_code.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read CSV file into pandas dataframe
+df = pd.read_csv("uzka_pro_orT01.csv")
+
+# Plot time-energy graph
+plt.plot(df["time"], df["KINETIC ENERGY"])
+plt.xlabel("Time")
+plt.ylabel("Energy")
+plt.show()
